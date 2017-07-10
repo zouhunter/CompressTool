36e6ccbf778d09a51b9d4ba6faf7ee0647b67354
zouhangte <zouhangtezbm@126.com>
2017-07-10 16:05:14 +0800

    修改压缩和解压的方式

--- SharpZipLib/ZipHelper.cs
+++ SharpZipLib/ZipHelper.cs
@@ -8,11 +8,371 @@ using ICSharpCode.SharpZipLib;
 using ICSharpCode.SharpZipLib.Zip;
 using ICSharpCode.SharpZipLib.Checksums;
 using ICSharpCode.SharpZipLib.Core;
-
+using System.Threading;
 
 public class ZipHelper
 {
-    #region 压缩文件
+    public delegate void FileChangeEvent(string filename,long total, long current);
+    /// <summary>
+    /// 多线程中压缩
+    /// </summary>
+    /// <param name="sourceFilePath"></param>
+    /// <param name="destinationZipFilePath"></param>
+    /// <param name="events"></param>
+    /// <param name="recurse"></param>
+    /// <param name="fileFilter"></param>
+    public static void CreateZipThread(string sourceFilePath, string destinationZipFilePath, FastZipEvents events, bool recurse = true, string fileFilter = "")
+    {
+        Thread thread = new Thread(new ThreadStart(()=> {
+            CreateZip(destinationZipFilePath, sourceFilePath, events,recurse,fileFilter);
+        }));
+
+        thread.Start();
+    }
+    
+    /// <summary>
+    /// 多线程指定添加文件
+    /// </summary>
+    /// <param name="sourceFilesPath"></param>
+    /// <param name="destinationZipFilePath"></param>
+    /// <param name="events"></param>
+    /// <param name="recurse"></param>
+    /// <param name="fileFilter"></param>
+    public static void CreateZipThread(Dictionary<string, string> sourceFilesPath, string destinationZipFilePath, FileChangeEvent events)
+    {
+        Thread thread = new Thread(new ThreadStart(() => {
+            CreateZip(sourceFilesPath, destinationZipFilePath, events);
+        }));
+
+        thread.Start();
+    }
+    
+    /// <summary>
+    /// 将指定文件压缩到zip文件
+    /// </summary>
+    /// <param name="sourceFilesPath"></param>
+    /// <param name="destinationZipFilePath"></param>
+    /// <param name="events"></param>
+    /// <param name="recurse"></param>
+    /// <param name="fileFilter"></param>
+    public static void CreateZip(Dictionary<string,string> sourceFilesPath, string destinationZipFilePath, FileChangeEvent events)
+    {
+        ZipOutputStream zipStream = new ZipOutputStream(File.Create(destinationZipFilePath));
+        zipStream.SetLevel(9);  // 压缩级别 0-9
+
+        Crc32 crc = new Crc32();
+        Dictionary<ZipEntry, byte[]> holders = new Dictionary<ZipEntry, byte[]>();
+        long total = 0;
+        long current = 0;
+        foreach (var file in sourceFilesPath)
+        {
+            if (File.Exists(file.Key))
+            {
+                FileStream fileStream = File.OpenRead(file.Key);
+                byte[] buffer = new byte[fileStream.Length];
+                fileStream.Read(buffer, 0, buffer.Length);
+                ZipEntry entry = new ZipEntry(file.Value);
+                entry.DateTime = DateTime.Now;
+                entry.Size = fileStream.Length;
+                entry.IsUnicodeText = true;
+                fileStream.Close();
+                crc.Reset();
+                crc.Update(buffer);
+                entry.Crc = crc.Value;
+                holders.Add(entry, buffer);
+                total += buffer.Length;
+            }
+        }
+
+        foreach (var hoder in holders)
+        {
+            zipStream.PutNextEntry(hoder.Key);
+            zipStream.Write(hoder.Value, 0, hoder.Value.Length);
+            if (events != null) events.Invoke(hoder.Key.Name, total, current += hoder.Value.Length);
+        }
+
+        zipStream.Finish();
+        zipStream.Close();
+    }
+
+    /// <summary>
+    /// 解压到指定文件夹
+    /// </summary>
+    /// <param name="zipFileName"></param>
+    /// <param name="targetDirectory"></param>
+    /// <param name="events"></param>
+    public static void ExtractZipThread(string zipFileName, string targetDirectory, FileChangeEvent events)
+    {
+        Thread thread = new Thread(new ThreadStart(() => {
+            ExtractZip(zipFileName, targetDirectory, events);
+        }));
+
+        thread.Start();
+    }
+    public static void ExtractZip(string zipFileName, string targetDirectory, FileChangeEvent events)
+    {
+        if (!File.Exists(zipFileName))
+        {
+            return;
+        }
+
+        if (!Directory.Exists(targetDirectory))
+        {
+            Directory.CreateDirectory(targetDirectory);
+        }
+
+        ZipInputStream s = null;
+        ZipEntry theEntry = null;
+
+        string fileName;
+        FileStream streamWriter = null;
+
+        try
+        {
+            long total = 0;
+            long current = 0;
+
+            s = new ZipInputStream(File.OpenRead(zipFileName));
+            Dictionary<string, byte[]> dataDic = new Dictionary<string, byte[]>();
+
+            while ((theEntry = s.GetNextEntry()) != null)
+            {
+                total += s.Length;
+                fileName = Path.Combine(targetDirectory, theEntry.Name);
+
+                if (fileName.EndsWith("/") || fileName.EndsWith("\\"))
+                {
+                    Directory.CreateDirectory(fileName);
+                    continue;//文件夹跳过
+                }
+
+                if (theEntry.Name != String.Empty)
+                {
+                    using (var mem = new MemoryStream())
+                    {
+                        int size = 2048;
+                        byte[] data = new byte[2048];
+                        while (true)
+                        {
+                            size = s.Read(data, 0, data.Length);
+                            if (size > 0)
+                            {
+                                mem.Write(data, 0, size);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        dataDic.Add(theEntry.Name, mem.ToArray());
+                    }
+                   
+                }
+            }
+            foreach (var item in dataDic)
+            {
+                fileName = Path.Combine(targetDirectory, item.Key);
+                ///判断文件路径是否是文件夹
+                if (!Directory.Exists(Path.GetDirectoryName(fileName)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(fileName));
+                }
+                current += item.Value.Length;
+                streamWriter = File.Create(fileName);
+                streamWriter.Write(item.Value, 0, item.Value.Length);
+                if (events != null) events.Invoke(item.Key, total, current);
+            }
+        }
+
+        catch
+        {
+            throw;
+        }
+        finally
+        {
+            if (streamWriter != null)
+            {
+                streamWriter.Close();
+                streamWriter = null;
+            }
+            if (theEntry != null)
+            {
+                theEntry = null;
+            }
+            if (s != null)
+            {
+                s.Close();
+                s = null;
+            }
+            GC.Collect();
+            GC.Collect(1);
+        }
+    }
+    /// <summary>
+    /// 多线程中解压
+    /// </summary>
+    /// <param name="sourceFilePath"></param>
+    /// <param name="destinationZipFilePath"></param>
+    /// <param name="events"></param>
+    /// <param name="recurse"></param>
+    /// <param name="fileFilter"></param>
+    public static void ExtractZipThread(string zipFileName, string targetDirectory, FastZipEvents events = null, string fileFilter = "")
+    {
+        Thread thread = new Thread(new ThreadStart(() => {
+            ExtractZip(zipFileName, targetDirectory, events,fileFilter);
+        }));
+
+        thread.Start();
+    }
+
+    /// <summary>
+    /// 多线程添加指定文件
+    /// </summary>
+    /// <param name="zipFileName"></param>
+    /// <param name="filesNames"></param>
+    public static void AddFileToZipThread(string zipFileName,Dictionary<string, string> filesNames)
+    {
+        Thread thread = new Thread(new ThreadStart(() => {
+            AddFileToZip(zipFileName, filesNames);
+        }));
+
+        thread.Start();
+    }
+
+    /// <summary>
+    /// 从指定zip删除文件
+    /// </summary>
+    /// <param name="zipFileName"></param>
+    /// <param name="fileNames"></param>
+    public static void DeleteFileFromZipThread(string zipFileName, IList<string> fileNames)
+    {
+        Thread thread = new Thread(new ThreadStart(() => {
+            DeleteFileFromZip(zipFileName, fileNames);
+        }));
+
+        thread.Start();
+    }
+
+    /// <summary>
+    /// 压缩
+    /// </summary>
+    /// <param name="sourceDirectory"></param>
+    /// <param name="targetZipName"></param>
+    /// <param name="recurse"></param>
+    /// <param name="filter"></param>
+    /// <returns></returns>
+    public static void CreateZip(string zipFileName, string sourceDirectory, FastZipEvents events = null, bool recurse = true, string fileFilter = "")
+    {
+        if (string.IsNullOrEmpty(sourceDirectory))
+        {
+            throw new ArgumentNullException("SourceZipDirectory");
+        }
+        if (string.IsNullOrEmpty(zipFileName))
+        {
+            throw new ArgumentNullException("TargetZipName");
+        }
+        if (!Directory.Exists(sourceDirectory))
+        {
+            throw new DirectoryNotFoundException("SourceDirecotry");
+        }
+        if (Path.GetExtension(zipFileName).ToUpper() != ".ZIP")
+            throw new ArgumentException("TargetZipName  is not zip");
+        FastZip fastZip = new FastZip(events);
+        fastZip.CreateZip(zipFileName, sourceDirectory, recurse, fileFilter);
+    }
+
+    /// <summary>
+    /// 解压
+    /// </summary>
+    /// <param name="zipFileName"></param>
+    /// <param name="targetDirectory"></param>
+    /// <param name="fileFilter"></param>
+    public static void ExtractZip(string zipFileName, string targetDirectory, FastZipEvents events = null, string fileFilter = "")
+    {
+        if (string.IsNullOrEmpty(zipFileName))
+        {
+            throw new ArgumentNullException("ZIPFileName");
+        }
+        if (!File.Exists(zipFileName))
+        {
+            throw new FileNotFoundException("zipFileName");
+        }
+        if (Path.GetExtension(zipFileName).ToUpper() != ".ZIP")
+        {
+            throw new ArgumentException("ZipFileName is not Zip ");
+        }
+        FastZip fastZip = new FastZip(events);
+        fastZip.ExtractZip(zipFileName, targetDirectory, fileFilter);
+    }
+
+    /// <summary>
+    /// 添加文件到压缩文件中
+    /// </summary>
+    /// <param name="zipFileName"></param>
+    /// <param name="filesNames"></param>
+    public static void AddFileToZip(string zipFileName, Dictionary<string, string> filesNames)
+    {
+        if (string.IsNullOrEmpty(zipFileName))
+        {
+            throw new ArgumentNullException("ZipName");
+        }
+        if (Path.GetExtension(zipFileName).ToUpper() != ".ZIP")
+        {
+            throw new ArgumentException("ZipFileName is not Zip ");
+        }
+        if (filesNames == null || filesNames.Count < 1)
+            return;
+
+        ZipFile zFile = File.Exists(zipFileName) ? new ZipFile(zipFileName):ZipFile.Create(zipFileName);
+
+        using (zFile)
+        {
+            zFile.BeginUpdate();
+
+            foreach (KeyValuePair<string,string> fileName in filesNames)
+            {
+                zFile.Add(fileName.Key,fileName.Value);
+                UnityEngine.Debug.Log(fileName.Key);
+            }
+
+            zFile.CommitUpdate();
+        }
+
+    }
+
+    /// <summary>
+    /// 移除压缩文件中的文件
+    /// </summary>
+    /// <param name="zipName"></param>
+    /// <param name="fileNames"></param>
+    public static void DeleteFileFromZip(string zipFileName, IList<string> fileNames)
+    {
+        if (string.IsNullOrEmpty(zipFileName))
+        {
+            throw new ArgumentNullException("ZipName");
+        }
+        if (Path.GetExtension(zipFileName).ToUpper() != ".ZIP")
+        {
+            throw new ArgumentException("ZipName");
+        }
+        if (fileNames == null || fileNames.Count < 1)
+        {
+            return;
+        }
+        using (ZipFile zipFile = new ZipFile(zipFileName))
+        {
+            zipFile.BeginUpdate();
+            foreach (string fileName in fileNames)
+            {
+                zipFile.Delete(fileName);
+            }
+            zipFile.CommitUpdate();
+        }
+    }
+}
+/*
+  #region 压缩文件
+
     /// <summary>
     /// 压缩文件
     /// </summary>
@@ -28,6 +388,7 @@ public class ZipHelper
         zipStream.Finish();
         zipStream.Close();
     }
+
     /// <summary>
     /// 递归压缩文件
     /// </summary>
@@ -146,4 +507,4 @@ public class ZipHelper
     }
 
     #endregion
-}
+     */
